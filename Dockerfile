50652ce48d162dafdc114c9346bd06411a5e9ffb
dev-onejun <wjjgjk@naver.com>
2023-12-31 14:42:43 +0900

    UPDATE: test scripts

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,16 @@
-FROM ruby:3.0.5-buster
+FROM ruby:3.0.5-slim-buster
 
 WORKDIR /usr/src/app
 
 COPY ./Gemfile .
 
-RUN gem install jekyll bundler
+RUN apt update && \
+    apt install ruby-dev gcc g++ libffi-dev make -y
+
+RUN gem install bundler && \
+    gem install jekyll
+
 
 RUN bundle install
+
+CMD ["bundle", "exec", "jekyll", "serve", "-H0.0.0.0", "--port=4000"]
--- test.sh
+++ test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 name=$(basename $PWD)
+jekyll_port=4000
 
 if [ -z $1 ]; then
     echo -e "Please give the port number that you want to run docker container"
 
 elif [ $1 = "clean" ]; then
-    sudo docker stop $name
-    sudo docker rm $name
-    sudo docker rmi test/$name:latest
+    docker stop $name
+    docker rm $name
+    docker rmi test/$name:latest
 
 elif [ $1 != "clean" ]; then
     if ! [[ $1 =~ ^[0-9]+$ ]]; then
@@ -16,21 +17,15 @@ elif [ $1 != "clean" ]; then
 
     # run test with the given port number
     elif [ $1 -ge 2000 ] && [ $1 -le 65536 ]; then
-        if [ -z "$(sudo docker images | grep test/$name)" ]; then
-            sudo docker build -t test/$name .
+        if [ -z "$(docker images | grep test/$name)" ]; then
+            docker build -t test/$name .
 
             echo -e "\n\n\n**************************" &&
             echo -e "test image is built successfully.\nIf you want to rebuild the image, Please run the command again with 'clean' option" &&
             echo -e "**************************"
         fi
 
-        bundle add webrick
-
-        jekyll_port=4000
-        sudo docker run --rm -it -p $1:$jekyll_port -v $PWD:/usr/src/app --name $name test/$name:latest bundle exec jekyll serve -H 0.0.0.0 --port=$jekyll_port
-
-        bundle remove webrick
-
+        docker run --rm -it -p $1:$jekyll_port -v $PWD:/usr/src/app --name $name test/$name:latest
 
     #elif [ $1 -lt 2000 ] && [ $1 -gt 65536 ]; then
     else
